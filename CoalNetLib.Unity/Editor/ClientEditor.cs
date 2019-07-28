48f35229162f0542c3142f2a03a888ab31595d71
NahoyGames <yohan.guyomard@gmail.com>
2019-07-27 20:11:20 -0700

    Client manager script gets a custom editor

---
+++ CoalNetLib.Unity/Editor/ClientEditor.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace CoalNetLib.Unity.Editor
+{
+    [CustomEditor(typeof(Client))]
+    public class ClientEditor : UnityEditor.Editor
+    {
+        private Client _client;
+
+        private void OnEnable()
+        {
+            _client = (Client) target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (!_client.Connected)
+            {
+                if (GUILayout.Button("Connect"))
+                {
+                    _client.Connect();
+                }
+            }
+            else if (_client.Connecting)
+            {
+                GUILayout.Button("Connecting...");
+            }
+            else
+            {
+                if (GUILayout.Button("Disconnect"))
+                {
+                    _client.Disconnect();
+                }
+            }
+        }
+    }
+}
